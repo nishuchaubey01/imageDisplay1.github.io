332f6ee0f2f2ea1582d8306f7b3060b215825108
nishuchaubey01 <84399572+nishuchaubey01@users.noreply.github.com>
2022-11-28 12:55:34 +0530

    image Display website

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+// import ImageDisplay from './COMPONENTS/STYLENUMBER/ImageDisplay';
+// import Stylenum from './COMPONENTS/STYLENUMBER/Stylenum';
+// import Display2 from './Display/Display2';
+import Display1 from './Display/Display1'
+// import Display from './Display/Display';
+
+function App() {
+  return (
+    <div className="App">
+     
+      {/* <Stylenum/> */}
+       {/* <ImageDisplay/>  */}
+       <Display1/> 
+       {/* <Display/> */}
+       {/* <Display2/> */}
+
+       
+      </div>
+  );
+}
+
+
+export default App;
